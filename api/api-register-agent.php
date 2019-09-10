afbd7222c3b79e0b0178db93e7a531257bb1ca0e
TijsM <tijs.martens@student.hogent.be>
2019-09-10 07:03:28 +0200

    added api-register-user

--- api/api-register-agent.php
+++ api/api-register-agent.php
@@ -4,12 +4,9 @@ $sUserName = $_POST['name'];
 $sFamilyName = $_POST['familyName'];
 $sEmail = $_POST['email'];
 $sPassword = $_POST['password'];
+$sPhone = $_POST['phoneNumber'];
 
-
-// echo $sUserName;
-// echo $sFamilyName;
-// echo $sEmail;
-// echo $sPassword;
+// echo $sPhone;
 
 if(empty($sUserName)){
     sendErrorMessage('name is not filled in', __LINE__);
@@ -23,14 +20,24 @@ if(empty($sEmail)){
 if(empty($sPassword)){
     sendErrorMessage('password is not filled in', __LINE__);
 }
-
+if(empty($sPhone)){
+    sendErrorMessage('phonenumber is not filled in', __LINE__);
+}
 
 if (!filter_var($sEmail, FILTER_VALIDATE_EMAIL)){
     sendErrorMessage('the mail has not the correct format', __line__);
 }
 
+if (strlen($sPhone) < 11){
+    sendErrorMessage('not a valid phone number, to short', __LINE__);
+}
 
 //validate the length of the password
+if (strlen($sPhone) > 13){
+    sendErrorMessage('not a valid phone number, to long', __line__);    
+}
+
+
 if (strlen($sPassword) < 6){
     sendErrorMessage('password to sort, must be at least 6 characters long', __LINE__);
 }
@@ -46,23 +53,23 @@ $sUsers = file_get_contents('../data.json');
 $jUsers = json_decode($sUsers);
 
 $sUniqueId = uniqid();
-$jUsers->users->$sUniqueId = new stdClass();
-$jUsers->users->$sUniqueId->id = $sUniqueId;
-$jUsers->users->$sUniqueId->name = $sUserName;
-$jUsers->users->$sUniqueId->lastName = $sFamilyName;
-$jUsers->users->$sUniqueId->email = $sEmail;
-$jUsers->users->$sUniqueId->password = $sPassword;
-
+$jUsers->agents->$sUniqueId = new stdClass();
+$jUsers->agents->$sUniqueId->id = $sUniqueId;
+$jUsers->agents->$sUniqueId->name = $sUserName;
+$jUsers->agents->$sUniqueId->lastName = $sFamilyName;
+$jUsers->agents->$sUniqueId->email = $sEmail;
+$jUsers->agents->$sUniqueId->password = $sPassword;
+$jUsers->agents->$sUniqueId->phone = $sPhone;
+$jUsers->agents->$sUniqueId->properties = new stdClass();
 
 $sUsers = json_encode($jUsers);
 file_put_contents('../data.json', $sUsers);
 
-
-$_SESSION['jUser'] = $jUsers->users->$sUniqueId;
+$_SESSION['jUser'] = $jUsers->agents->$sUniqueId;
 
 echo '{
     "status": 1 ,
-    "message": "user was registred"
+    "message": "agent was registred"
 }';
 
 
