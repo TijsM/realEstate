0a7aaa23ab7fc3fc6addd72e39209085e24ddfb0
TijsM <tijs.martens@student.hogent.be>
2019-09-12 20:17:49 +0200

    map has all functions

--- properties.php
+++ properties.php
@@ -14,13 +14,7 @@ $jProperties = $jData->properties;
 <div id="propertiesPage">
     <div id="mapContainer">
         <div id='map'></div>
-        <script>
-            mapboxgl.accessToken = 'pk.eyJ1IjoidGlqc21hcnRlbnMiLCJhIjoiY2pweWNiMHF4MHJ0NTN5bXZzNnFqYjAxbSJ9.on7oKIYiOGKo2sK4qnXqag';
-            var map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11'
-            });
-        </script>
+
     </div>
     <div id="propertiesContainer">
         <!-- 
@@ -41,25 +35,88 @@ $jProperties = $jData->properties;
         foreach ($jProperties as $jProp) {
             $firstImage = "assets/uploadedProperties/{$jProp->images[0]}";
             echo '
-            <div class="card propertyCard">
-                
+            <div class="card propertyCard" id="prop' . $jProp->propertyId . '">
                 <img src="' . $firstImage . '" class="card-img-top" alt="img house">
                 <div class="card-body">
                     <h3 class="card-title">' . $jProp->name . '</h3>
                     <h5>€' . $jProp->price . '</h5>
                     <p> bedrooms: ' . $jProp->bedrooms . '</p>
                     <p>' . $jProp->location->city . ' - ' . $jProp->location->street . '</p>
-                    <a class="btn btn-primary" href="property-details.php?id='.$jProp->propertyId.'" role="button">
+                    <a class="btn btn-primary" href="property-details.php?id=' . $jProp->propertyId . '" role="button">
                         view details
                     </a>
                 </div>
             </div> 
+            
+            <script>
+            card = document.getElementById("prop' . $jProp->propertyId . '")
+            console.log("' . $jProp->propertyId . '")
+            console.log(card);
+            card.addEventListener("click", function(){
+                removeActiveClassFromProperty()
+    
+                console.log( document.getElementById("marker"+"' . $jProp->propertyId . '"))
+                document.getElementById("marker"+"' . $jProp->propertyId . '").classList.add("active")
+                document.getElementById("prop"+"' . $jProp->propertyId . '").classList.add("active")
+            })
+            </script>
+
+            
             ';
         }
         ?>
     </div>
 </div>
 
+
+<script>
+    //creating the map
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGlqc21hcnRlbnMiLCJhIjoiY2pweWNiMHF4MHJ0NTN5bXZzNnFqYjAxbSJ9.on7oKIYiOGKo2sK4qnXqag';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [12.555050, 55.70], // starting position
+        zoom: 12, // starting zoom
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+
+
+    const sjProperties = '<?php echo json_encode($jProperties); ?>'
+    //creating an array of json objects
+    ajProperties = JSON.parse(sjProperties) // convert text into an object
+    console.log(ajProperties)
+
+
+    //looping over the json file
+    for (let propertyId in ajProperties) {
+        var el = document.createElement('i');
+
+        // console.log(propertyId);
+        // console.log(ajProperties);
+        // console.log(ajProperties[propertyId].propertyId);
+        el.href = 'prop' + ajProperties[propertyId].propertyId;
+        el.className = 'fas fa-home fa-2x';
+        el.style.color = 'black';
+        el.id = 'marker' + ajProperties[propertyId].propertyId;
+        let cords = [parseFloat(ajProperties[propertyId].location.longtitude), parseFloat(ajProperties[propertyId].location.latitude)]
+        new mapboxgl.Marker(el).setLngLat(cords).addTo(map);
+
+        el.addEventListener('click', function() {
+            removeActiveClassFromProperty()
+
+            console.log(document.getElementById('marker' + ajProperties[propertyId].propertyId))
+            document.getElementById('marker' + ajProperties[propertyId].propertyId).classList.add('active')
+            document.getElementById('prop' + ajProperties[propertyId].propertyId).classList.add('active')
+        })
+    }
+
+    function removeActiveClassFromProperty() {
+        let properties = document.querySelectorAll('.active')
+        properties.forEach(function(oPropertyDiv) {
+            oPropertyDiv.classList.remove('active')
+        })
+    }
+</script>
 
 <?php
 require_once(__DIR__ . '/components/bottom.php');
